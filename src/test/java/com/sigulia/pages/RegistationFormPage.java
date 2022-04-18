ad19f611b9a953bbe0893b3010b3478773e48b3f
Daria <sigulia39@gmail.com>
2022-04-18 23:31:21 +0300

    PageObject

---
+++ src/test/java/com/sigulia/pages/RegistationFormPage.java
@@ -0,0 +1,70 @@
+package com.sigulia.pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class RegistationFormPage {
+
+    // locators
+    SelenideElement firstName = $("#firstName"),
+                    lastName = $("#lastName"),
+                    userEmail = $("#userEmail"),
+                    mobileNumber = $("#userNumber"),
+                    genter = $("#genterWrapper"),
+                    subjects = $("#subjectsInput"),
+                    address = $("#currentAddress"),
+                    city = $("#city"),
+                    state = $("#state");
+
+
+
+    // actions
+    public RegistationFormPage openPage () {
+        open("/automation-practice-form");
+        return this;
+    }
+
+
+    public RegistationFormPage setFirstName(String value) {
+        firstName.setValue(value);
+        return this;
+    }
+
+    public RegistationFormPage setLastName(String value) {
+        lastName.setValue(value);
+        return this;
+    }
+
+    public RegistationFormPage setUserEmail(String value) {
+        userEmail.setValue(value);
+        return this;
+    }
+
+    public RegistationFormPage setMobileNumber(String value) {
+        mobileNumber.setValue(value);
+        return this;
+    }
+    public RegistationFormPage setAddress(String value) {
+        address.setValue(value);
+        return this;
+    }
+
+    public RegistationFormPage clickOnSubmitButton() {
+        $("#submit").click();
+        $(".modal-content").shouldBe(visible);
+        return this;
+    }
+
+    public RegistationFormPage checkResult(String key, String value) {
+        $(".table-responsive").$(byText(key))
+                .parent().shouldHave(text(value));
+        return this;
+    }
+
+
+}
--- src/test/java/com/sigulia/test/DemoQaTestsFakerData.java
+++ src/test/java/com/sigulia/test/DemoQaTestsFakerData.java
@@ -1,15 +1,13 @@
 package com.sigulia.test;
 import com.codeborne.selenide.Configuration;
 import com.github.javafaker.Faker;
+import com.sigulia.pages.RegistationFormPage;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.byXpath;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
 import static java.lang.String.format;
 
 
@@ -18,12 +16,9 @@ public class DemoQaTestsFakerData {
     @BeforeAll
     static void setUp() {
         Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
     }
 
-    @BeforeEach
-    void openPage() {
-        open("https://demoqa.com/automation-practice-form");
-    }
 
     Faker faker = new Faker();
 
@@ -38,18 +33,18 @@ public class DemoQaTestsFakerData {
             city = "Delhi";
     String expectedFullName = format("" + first_name + " " + last_name + "");
 
-    @Test
-    void submitWithoutValue() {
-        $("#submit").click();
-        $(".modal-content").shouldNotBe(visible);
-    }
+    RegistationFormPage registationFormPage = new RegistationFormPage();
+
 
     @Test
     void fillFormsWithObligatoryFields() {
-        $("#firstName").setValue(first_name);
-        $("#lastName").setValue(last_name);
-        $("#userEmail").setValue(user_email);
-        $("#userNumber").setValue(mobile_number);
+        registationFormPage.openPage()
+                .setFirstName(first_name)
+                .setLastName(last_name)
+                .setUserEmail(user_email)
+                .setMobileNumber(mobile_number)
+                .setAddress(address);
+
         $("#genterWrapper").$(byText(gender)).click();
         $("#dateOfBirthInput").click();
         $(".react-datepicker__year-select").find(byText("1999")).click();
@@ -59,19 +54,16 @@ public class DemoQaTestsFakerData {
         $(".subjects-auto-complete__option").click();
         $("#hobbiesWrapper .custom-control").click();
         $("#uploadPicture").uploadFromClasspath("cute_cat.jpg");
-        $("#currentAddress").setValue(address);
+
         $("#state").click();
         $("#stateCity-wrapper").$(byText(state)).click();
         $("#city").click();
         $("#stateCity-wrapper").$(byText(city)).click();
-        $("#submit").click();
-        $(".modal-content").shouldBe(visible);
-        $(byXpath("//td[contains(.,'Student Name')]/following-sibling::td")).shouldHave(text(expectedFullName));
-        $(byXpath("//td[contains(.,'Student Name')]/following-sibling::td")).shouldHave(text(expectedFullName));
-        $(byXpath("//td[contains(.,'Student Email')]/following-sibling::td")).shouldHave(text(user_email));
-        $(byXpath("//td[contains(.,'Gender')]/following-sibling::td")).shouldHave(text(gender));
-        $(byXpath("//td[contains(.,'Mobile')]/following-sibling::td")).shouldHave(text(mobile_number));
-        $("#closeLargeModal").click();
-
+        registationFormPage.clickOnSubmitButton()
+                .checkResult("Student Name", expectedFullName)
+                .checkResult("Student Email", user_email)
+                .checkResult("Mobile", mobile_number)
+                .checkResult("Address", address)
+                .checkResult("Gender", gender);
     }
 }
